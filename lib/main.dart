1550f4dd2f9a9a9328f995f16f3655f835ce514c
alabbas2002 <alabbasas12340@gmail.com>
2025-01-15 22:06:53 +0300

    gg

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      debugShowCheckedModeBanner: 5 == 4 ? true : false,
+      debugShowCheckedModeBanner: false,
       home: Home(),
     );
   }
