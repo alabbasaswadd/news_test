d97916f58419a66824d2f45c2f080679fd80b1a7
alabbas2002 <alabbasas12340@gmail.com>
2025-01-16 17:11:56 +0300

    in branch ana

---
+++ lib/in_ana_branch.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AnaBranch extends StatelessWidget {
+  const AnaBranch({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("aa");
+  }
+}
